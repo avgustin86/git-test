b9451a1eea6a2b7ef01e8fed5082e2c681b2e16f
Avgustin <soundgraf@mail.ru>
2022-10-10 01:57:14 +0300

    Исправил по комментариям

--- com/avgustin/Main.java
+++ com/avgustin/Main.java
@@ -4,7 +4,6 @@ import com.avgustin.lesson1.Homework1;
 
 public class Main {
     public static void main(String[] args) {
-    Homework1 homework1 = new Homework1();
-    homework1.myMethod1();
+    Homework1.myMethod1();
     }
 }
