9542b99dcc27db9e799764e127676c5963190923
Avgustin <soundgraf@mail.ru>
2022-10-10 02:31:04 +0300

    Homework3 Commit

--- com/avgustin/lesson2/Homework2.java
+++ com/avgustin/lesson2/Homework2.java
@@ -1,7 +1,10 @@
 package com.avgustin.lesson2;
 
+import com.avgustin.lesson3.Homework3;
+
 public class Homework2 {
     public static void myMethod2() {
         System.out.println("Method2 Done!");
+        Homework3.myMethod3();
     }
 }
---
+++ com/avgustin/lesson3/Homework3.java
@@ -0,0 +1,7 @@
+package com.avgustin.lesson3;
+
+public class Homework3 {
+    public static void myMethod3() {
+        System.out.println("Method3 Done!");
+    }
+}
