a9f475111bedfcde0ea018564848e2bbef95c905
Avgustin <soundgraf@mail.ru>
2022-10-10 02:38:43 +0300

    Homework5&4 Commit!

--- com/avgustin/lesson4/Homework4.java
+++ com/avgustin/lesson4/Homework4.java
@@ -1,7 +1,10 @@
 package com.avgustin.lesson4;
 
+import com.avgustin.lesson5.Homework5;
+
 public class Homework4 {
     public static void myMethod4() {
-        System.out.println("Method4 Done!!!!!!");
+        System.out.println("Method4 Done!");
+        Homework5.myMethod5();
     }
 }
